f5790d6774e340e358ea99a8327ce85569e78e90
Top Magar <magartop150@gmail.com>
2026-04-18 15:35:45 +0545

    editor step 1: data model + hardcoded render - hero, 2-col features, image

--- app/(main)/sub-account/[subAccountId]/funnels/[funnelId]/editor/[funnelPageId]/page.tsx
+++ app/(main)/sub-account/[subAccountId]/funnels/[funnelId]/editor/[funnelPageId]/page.tsx
@@ -1,26 +1,5 @@
-import { db } from "@/lib/db";
-import { notFound } from "next/navigation";
-import EditorClient from "./editor-client";
+import EditorStep1 from "@/components/editor/editor-step1";
 
-export default async function EditorPage({
-  params,
-}: {
-  params: Promise<{ subAccountId: string; funnelId: string; funnelPageId: string }>;
-}) {
-  const { subAccountId, funnelId, funnelPageId } = await params;
-
-  const funnelPage = await db.funnelPage.findUnique({
-    where: { id: funnelPageId },
-  });
-
-  if (!funnelPage) return notFound();
-
-  return (
-    <EditorClient
-      subAccountId={subAccountId}
-      funnelId={funnelId}
-      funnelPageId={funnelPageId}
-      pageDetails={funnelPage}
-    />
-  );
+export default async function EditorPage() {
+  return <EditorStep1 />;
 }
